b37120185e706132f57a1fd47c882a41336caac4
Rohit Sharma <rs1257933@gmail.com>
2020-06-23 18:35:49 +0530

    remove punction

---
+++ CURD/views.py
@@ -0,0 +1,57 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Code for pipeline
+# def index(request):
+#     return HttpResponse('''yoo yo banta raper''')
+
+def about(request):
+    return HttpResponse("About ")
+    
+def removepunc(request):
+    #Get the text
+    djtext = request.GET.get('text', 'default')
+    print(djtext)
+    #Analyze the text
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("capitalize first")
+
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+def charcount(request):
+    return HttpResponse("charcount ")    
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def analyze(request):
+    #Get the text
+    djtext = request.GET.get('text', 'default')
+
+    # Check checkbox values
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    else:
+        params = {'purpose':'with Punctuations', 'analyzed_text': djtext } 
+        return render(request, 'analyze.html', params)
