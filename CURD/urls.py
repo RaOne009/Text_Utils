e331c63dbea3981e99e140d4efd99ee71484d220
Rohit Sharma <rs1257933@gmail.com>
2020-06-24 11:13:54 +0530

    added new feature

--- CURD/urls.py
+++ CURD/urls.py
@@ -17,23 +17,33 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-       path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-]
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#        path('', views.index, name='index'),
+#     path('about/', views.about, name='about'),
+# ]
 
 
 # Code for pipeline
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.index, name='index'),
+#     path('removepunc', views.removepunc, name='rempun'),
+#     path('capitalizefirst', views.capfirst, name='capfirst'),
+#     path('newlineremove', views.newlineremove, name='newlineremove'),
+#     path('spaceremove', views.spaceremove, name='spaceremove'),
+#     path('charcount', views.charcount, name='charcount'),
+
+#     path('analyze', views.analyze, name='analyze')
+
+# ]
+
+# text utiles
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('removepunc', views.removepunc, name='rempun'),
-    path('capitalizefirst', views.capfirst, name='capfirst'),
-    path('newlineremove', views.newlineremove, name='newlineremove'),
-    path('spaceremove', views.spaceremove, name='spaceremove'),
-    path('charcount', views.charcount, name='charcount'),
+    path('analyze', views.analyze, name='analyze'),
+    path('ex1', views.ex1, name='ex1'),
 
-    path('analyze', views.analyze, name='analyze')
 
 ]
