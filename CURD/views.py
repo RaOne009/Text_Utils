53fcaa896b6ad85d9a415feb21092ba45d09dffc
Rohit Sharma <rs1257933@gmail.com>
2020-06-26 11:01:04 +0530

    FINESHED

--- CURD/views.py
+++ CURD/views.py
@@ -41,49 +41,52 @@ def analyze(request):
     fullcaps = request.POST.get('fullcaps', 'off')
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST
+    
+    #Check which checkbox is on
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
+
         params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    
-    elif(fullcaps=="on"):
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    if(fullcaps=="on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
 
         params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
         # Analyze the text
-        return render(request, 'analyze.html', params)
-
-    elif(extraspaceremover=="on"):
+        # return render(request, 'analyze.html', params)
+    if(extraspaceremover == "on"):
         analyzed = ""
         for index, char in enumerate(djtext):
-            if not(djtext[index] == " " and djtext[index+1]==" "):
+            if not(djtext[index] == " " and djtext[index+1] == " "):
                 analyzed = analyzed + char
 
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        params = {'purpose': 'Removed extraspace', 'analyzed_text': analyzed}
+        djtext = analyzed
         # Analyze the text
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
 
-    elif (newlineremover == "on"):
+    if (newlineremover == "on"):
         analyzed = ""
         for char in djtext:
-            if char != "\n":
+            if char != "\n" and char!="\r":
                 analyzed = analyzed + char
-
+            else:
+                print("no")
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
 
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+        return HttpResponse("please select any operation and try again")
 
-    else:
-        params = {'purpose': 'pta ni', 'analyzed_text': djtext}
-        return render(request, 'analyze.html', params)
-
+    return render(request, 'analyze.html', params)
 
 def ex1(request):
     sites = ['''For Entertainment youtube video''',
